092f091e347cd92a3c06186e0cd001e886d9febf
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-10-09 19:35:16 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,26 @@ const header = document.createElement("h1");
 header.innerHTML = GAMENAME;
 app.append(header);
 
+
+
+// button
 const start_button = document.createElement("button");
-start_button.innerHTML = "🥤🥗🍔🍗🍟🥓";
+start_button.innerHTML = "🍔";
 start_button.className = "button"; // for style
 app.append(start_button);
+
+
+// create a div element
+const burger_counter_element = document.createElement("div");
+burger_counter_element.innerHTML = '0 burgers';
+app.append(burger_counter_element);
+
+
+// changing
+let burger_cnt: number = 0;
+
+start_button.addEventListener("click", () => {
+    console.log("Burger button clicked!");
+    burger_cnt++
+    burger_counter_element.innerHTML = `${burger_cnt} burgers`;
+});
