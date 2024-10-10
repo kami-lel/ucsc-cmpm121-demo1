6da40085f554a53a70bb6eea6a392e9cf7697c88
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-10-09 20:52:32 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,14 @@ const header = document.createElement("h1");
 header.innerHTML = GAMENAME;
 app.append(header);
 
+
+let burger_cnt: number = 0;
+let growth_mux: number = 0;
 // button
 const start_button = document.createElement("button");
 start_button.innerHTML = "🍔";
 start_button.className = "button"; // for style
 app.append(start_button);
-
-// create a div element
-const burger_counter_element = document.createElement("div");
-burger_counter_element.innerHTML = "0 burgers";
-app.append(burger_counter_element);
-
-// changing
-let burger_cnt: number = 0;
 
 start_button.addEventListener("click", () => {
   console.log("Burger button clicked!");
@@ -31,11 +26,45 @@ start_button.addEventListener("click", () => {
 });
 
 
-function auto_increase() {
-  burger_cnt++;
-  burger_counter_element.innerHTML = `${burger_cnt} burgers`;
-  requestAnimationFrame(auto_increase)
+const PURCHASE_COST: number = 10
+const purchase_button = document.createElement("button");
+purchase_button.innerHTML = "Buy";
+purchase_button.className = "button"; // for style
+purchase_button.disabled = true; // disable the button
+app.append(purchase_button);
+
+
+purchase_button.addEventListener("click", () => {
+  console.log("purchase button clicked!");
+  burger_cnt -= PURCHASE_COST;
+  growth_mux += 1;
+});
+
+
+
+// create a div element
+const burger_counter_element = document.createElement("div");
+burger_counter_element.innerHTML = "0 burgers";
+app.append(burger_counter_element);
+
+let _last_time: number = performance.now();
+let _time_delta: number = 0;
+
+function update() {
+    _time_delta = (performance.now() - _last_time) / 1000
+    _last_time = performance.now()
+
+    if (burger_cnt >= PURCHASE_COST) {
+        purchase_button.disabled = false; // enable the button
+    } else {
+        purchase_button.disabled = true; // disable the button
+    }
+
+    burger_cnt += growth_mux * _time_delta;
+    burger_counter_element.innerHTML = `${burger_cnt.toFixed(3)} burgers`;
+
+    requestAnimationFrame(update)
 }
 
+requestAnimationFrame(update);
 
-requestAnimationFrame(auto_increase)
