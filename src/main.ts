1a29993ce69ea19f6bca4ae23eeaa21f75bffeed
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-10-09 19:44:26 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -10,26 +10,29 @@ const header = document.createElement("h1");
 header.innerHTML = GAMENAME;
 app.append(header);
 
-
-
 // button
 const start_button = document.createElement("button");
 start_button.innerHTML = "🍔";
 start_button.className = "button"; // for style
 app.append(start_button);
 
-
 // create a div element
 const burger_counter_element = document.createElement("div");
-burger_counter_element.innerHTML = '0 burgers';
+burger_counter_element.innerHTML = "0 burgers";
 app.append(burger_counter_element);
-
 
 // changing
 let burger_cnt: number = 0;
 
 start_button.addEventListener("click", () => {
-    console.log("Burger button clicked!");
-    burger_cnt++
-    burger_counter_element.innerHTML = `${burger_cnt} burgers`;
+  console.log("Burger button clicked!");
+  burger_cnt++;
+  burger_counter_element.innerHTML = `${burger_cnt} burgers`;
 });
+
+
+// infinite loop using setInterval
+setInterval(() => {
+    burger_cnt++;
+    burger_counter_element.innerHTML = `${burger_cnt} burgers`;
+}, 1000); // updates every 1000 ms (1 second)
