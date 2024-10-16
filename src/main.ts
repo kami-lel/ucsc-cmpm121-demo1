fd8122e4f666de6f3fd274ef01af83aaaec35fdc
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-10-15 17:26:50 -0700

    Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,191 @@
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
+// constant declaration
 const GAMENAME = "My not so amazing game";
+const INIT_BURGER_CNT = 1000;
+const PURCHASE_A_COST = 10;
+const PURCHASE_B_COST = 100;
+const PURCHASE_C_COST = 1000;
 
+// set up HTML document & app
 document.title = GAMENAME;
-
-// create header
+const app: HTMLDivElement = document.querySelector("#app")!;
 const header = document.createElement("h1");
 header.innerHTML = GAMENAME;
 app.append(header);
 
-let burger_cnt: number = 0;
-let growth_mux: number = 0;
-// button
-const start_button = document.createElement("button");
-start_button.innerHTML = "🍔";
-start_button.className = "button"; // for style
-app.append(start_button);
+// create count number
+const burger_count = {
+  html_element: document.createElement("div"),
+  value: INIT_BURGER_CNT,
 
-start_button.addEventListener("click", () => {
+  update(): void {
+    this.html_element.innerText = `${this.value.toFixed(5)} burgers`;
+  },
+};
+app.append(burger_count.html_element);
+
+// burger button
+const burger_button_element = document.createElement("button");
+burger_button_element.innerText = "🍔";
+burger_button_element.className = "button"; // for style
+
+burger_button_element.addEventListener("click", () => {
   console.log("Burger button clicked!");
-  burger_cnt++;
-  burger_counter_element.innerHTML = `${burger_cnt} burgers`;
+  burger_count.value++;
 });
 
-const PURCHASE_COST: number = 10;
-const purchase_button = document.createElement("button");
-purchase_button.innerHTML = "Buy";
-purchase_button.className = "button"; // for style
-purchase_button.disabled = true; // disable the button
-app.append(purchase_button);
+app.append(burger_button_element);
 
-purchase_button.addEventListener("click", () => {
-  console.log("purchase button clicked!");
-  burger_cnt -= PURCHASE_COST;
-  growth_mux += 1;
-});
+// upgradable
+const upgradable_table_element = document.createElement("table");
+upgradable_table_element.innerHTML = `
+  <thead>
+    <tr>
+      <th></th>
+      <th>Purchase</th>
+      <th>Owned</th>
+      <th>Contribution\n(burges/sec)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>A</td>
+      <td>
+        <button class='button' id='button-a'>
+          ${PURCHASE_A_COST} burgers/purchase
+        </button>
+      </td>
+      <td id='count-a'></td>
+      <td id='rate-a'></td>
+    </tr>
+    <tr>
+      <td>B</td>
+      <td>
+        <button class='button' id='button-b'>
+          ${PURCHASE_B_COST} burgers/purchase
+        </button>
+      </td>
+      <td id='count-b'></td>
+      <td id='rate-b'></td>
+    </tr>
+    <tr>
+      <td>C</td>
+      <td>
+        <button class='button' id='button-c'>
+          ${PURCHASE_C_COST} burgers/purchase
+        </button>
+      </td>
+      <td id='count-c'></td>
+      <td id='rate-c'></td>
+    </tr>
+    <tr>
+      <td>total</td>
+      <td></td>
+      <td></td>
+      <td id='rate-total'></td>
+    </tr>
+  </tbody>
+`;
+app.append(upgradable_table_element);
 
-// create a div element
-const burger_counter_element = document.createElement("div");
-burger_counter_element.innerHTML = "0 burgers";
-app.append(burger_counter_element);
+class Upgradable {
+  count: number = 0;
+  cost: number;
+  name: string;
 
-let _last_time: number = performance.now();
-let _time_delta: number = 0;
+  multiplier: number;
+  button_element: HTMLButtonElement | null;
+  count_element: HTMLElement | null;
+  rate_element: HTMLElement | null;
 
-function update() {
-  _time_delta = (performance.now() - _last_time) / 1000;
-  _last_time = performance.now();
+  constructor(cost: number, multiplier: number, name: string) {
+    this.cost = cost;
+    this.multiplier = multiplier;
+    this.name = name;
 
-  if (burger_cnt >= PURCHASE_COST) {
-    purchase_button.disabled = false; // enable the button
-  } else {
-    purchase_button.disabled = true; // disable the button
+    this.button_element = document.getElementById(`button-${name}`) as HTMLButtonElement;
+    if (this.button_element) {
+      this.button_element.addEventListener("click", () => {
+        console.log(`purchase button ${this.name} clicked!`);
+        burger_count.value -= this.cost;
+        this.count++;
+      });
+    }
+
+    this.count_element = document.getElementById(`count-${name}`);
+    this.rate_element = document.getElementById(`rate-${name}`);
   }
 
-  burger_cnt += growth_mux * _time_delta;
-  burger_counter_element.innerHTML = `${burger_cnt.toFixed(3)} burgers`;
+  get per_sec_rate(): number {
+    return this.count * this.multiplier;
+  }
+
+  calc_addition(time_delta: number): number {
+    return time_delta * this.per_sec_rate;
+  }
+
+  update(): void {
+    if (this.button_element) {
+      this.button_element.disabled = burger_count.value < this.cost;
+    }
+    if (this.count_element) {
+      this.count_element.innerText = this.count.toString();
+    }
+    if (this.rate_element) {
+      this.rate_element.innerText = `${this.per_sec_rate.toFixed(3)}`;
+    }
+  }
+}
+
+const upgradable_a = new Upgradable(PURCHASE_A_COST, 0.1, "a");
+const upgradable_b = new Upgradable(PURCHASE_B_COST, 2, "b");
+const upgradable_c = new Upgradable(PURCHASE_C_COST, 50, "c");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// main update loop
+
+let _last_time: number = performance.now();
+let time_delta: number = 0;
+
+const total_rate_element = document.getElementById("rate-total");
+if (!total_rate_element) {
+  console.error("Total rate element not found.");
+}
+
+function update() {
+  time_delta = (performance.now() - _last_time) / 1000; // sec since last update
+  _last_time = performance.now();
+
+  upgradable_a.update();
+  upgradable_b.update();
+  upgradable_c.update();
+  burger_count.update();
+
+  const total_addition =
+    upgradable_a.calc_addition(time_delta) +
+    upgradable_b.calc_addition(time_delta) +
+    upgradable_c.calc_addition(time_delta);
+    burger_count.value += total_addition;
+
+  const total_rate =
+    upgradable_a.per_sec_rate +
+    upgradable_b.per_sec_rate +
+    upgradable_c.per_sec_rate;
+  if (total_rate_element) {
+    total_rate_element.innerText = `${total_rate.toFixed(3)}`;
+  }
 
   requestAnimationFrame(update);
 }
